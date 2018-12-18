41f977f236a2213d678dfd9ccc8ee06919d6fe63
Yuuna <acerola.love@gmail.com>
2018-12-18 23:00:17 +0900

    layout design

--- app/src/main/java/com/example/yuuna/finalprojectlibrary_searcher/creditActivity.java
+++ app/src/main/java/com/example/yuuna/finalprojectlibrary_searcher/creditActivity.java
@@ -1,19 +1,13 @@
 package com.example.yuuna.finalprojectlibrary_searcher;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
-public class MainActivity extends AppCompatActivity {
-
+public class creditActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_credit);
     }
 }
